56d04a5ff24ea6878784c52c5af5515c0a56ead8
강경훈 <kyunghun@gang-gyeonghuns-MacBook-Pro.local>
2022-02-15 20:06:32 +0900

    built-in http server

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-console.log("hello")
+const fs = require("fs");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+  const httpMethod = req.method;
+
+  if (pathName === "/profile") {
+    if (httpMethod === "GET") {
+      fs.readFile(`${__dirname}/data/profile.json`, "utf-8", (err, data) => {
+        res.writeHead(200, {
+          "Content-type": "application/json",
+        });
+        res.end(data);
+      });
+    }
+  } else if (pathName === "/feeds") {
+    if (httpMethod === "GET") {
+      fs.readFile(`${__dirname}/data/feeds.json`, "utf-8", (err, data) => {
+        const profileData = JSON.parse(data);
+
+        res.writeHead(200, {
+          "Content-type": "application/json",
+        });
+        res.end(JSON.stringify({ result: profileData }));
+      });
+    } else if (httpMethod === "POST") {
+      req.on("data", (bodyData) => {
+        console.log(bodyData.toString());
+
+        fs.readFile(`${__dirname}/data/feeds.json`, "utf-8", (err, data) => {
+          const feedData = JSON.parse(data);
+          const result = feedData.push(bodyData.toJSON("utf-8"));
+
+          console.log(feedData);
+        });
+
+        res.writeHead(200, {
+          "Content-type": "application/json",
+        });
+        res.end(bodyData);
+      });
+    }
+  } else {
+    res.writeHead(404, {
+      "Content-Type": "text/html",
+    });
+    res.end("<h1>NOT FOUND</h1>");
+  }
+});
+
+server.listen(8002, "127.0.0.1", () => {
+  console.log("Server started");
+});
