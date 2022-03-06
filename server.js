1341d8053752ff834d20e1d0cb48bd7aa665d267
강경훈 <kyunghun@gang-gyeonghuns-MacBook-Pro.local>
2022-03-06 16:55:46 +0900

    ADD: create posting api

--- server.js
+++ server.js
@@ -42,12 +42,30 @@ const signIn = async (req, res) => {
   }
 };
 
+const createPosting = async (req, res) => {
+  const data = req.body;
+  const headers = req.headers;
+
+  const query = await pool.query(
+    `INSERT INTO postings(title, content, user_id) VALUES('${data.title}', '${data.content}', ${headers.authorization});`
+  );
+
+  res.status(201).json({
+    status: "created",
+    data: {
+      insertedId: query[0].insertedId,
+    },
+  });
+};
+
 app.get("/ping", (req, res) => {
+  console.log(req.headers);
   res.status(200).json({ ping: "pong" });
 });
 
 app.post("/api/v1/users/signup", signUp);
 app.post("/api/v1/users/signin", signIn);
+app.post("/api/v1/postings", createPosting);
 
 const port = 3000;
 app.listen(port, () => {
